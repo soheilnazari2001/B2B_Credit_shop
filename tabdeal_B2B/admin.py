6b07fe630906efdbc21eab413b01312ce0f1a2de
soheilnazari2001 <s.nazari80820@gmail.com>
2023-08-08 12:15:45 +0430

    admin setting adjusted

--- tabdeal_B2B/admin.py
+++ tabdeal_B2B/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import *
+
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'credits']
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'seller', 'phone_number', 'amount', 'timestamp']
--- tabdeal_B2B/models.py
+++ tabdeal_B2B/models.py
@@ -5,9 +5,15 @@ class Seller(models.Model):
     name = models.CharField(max_length=100)
     credits = models.FloatField(default=0.0)
 
+    def __str__(self):
+        return f'{self.name} - {self.credits}'
+
 
 class Transaction(models.Model):
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15)
     amount = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.seller.name} - {self.phone_number} - {self.amount} - {self.timestamp}'
